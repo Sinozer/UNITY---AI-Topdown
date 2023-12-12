4e81b8bdb394df719c80fca046001ab86df29ddc
Théo <92313024+Sinozer@users.noreply.github.com>
2023-12-12 11:05:38 +0100

    feat: Added the first sample of the scenemanager

---
+++ Assets/Code/Scripts/Utils/SceneManager.cs
@@ -0,0 +1,39 @@
+// --------------------------------------- //
+// --------------------------------------- //
+//  Creation Date: 12/12/23
+//  Description: AI - Topdown
+// --------------------------------------- //
+// --------------------------------------- //
+
+public class SceneManager : Singleton<SceneManager>
+{
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadSceneAdditive(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName, UnityEngine.SceneManagement.LoadSceneMode.Additive);
+    }
+
+    public void LoadSceneAdditive(int sceneIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex, UnityEngine.SceneManagement.LoadSceneMode.Additive);
+    }
+
+    public void UnloadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+    public void UnloadScene(int sceneIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(sceneIndex);
+    }
+}
