eaa769a5f61af0ff9c86d2012a242705a6c17d8e
Evoider <everfrithza@gmail.com>
2023-12-13 12:48:49 +0100

    feat : player done for now

--- Assets/Code/Scripts/Player/FollowCursor.cs
+++ Assets/Code/Scripts/Player/FollowCursor.cs
@@ -11,6 +11,19 @@ public class FollowCursor : MonoBehaviour
     private void Update()
     {
         Vector2 mousePosition = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        // Get the screen boundaries
+        float screenX = Screen.width;
+        float screenY = Screen.height;
+
+        // Convert screen boundaries to world space
+        Vector3 minScreenBounds = _camera.ScreenToWorldPoint(new Vector3(0, 0, _camera.nearClipPlane));
+        Vector3 maxScreenBounds = _camera.ScreenToWorldPoint(new Vector3(screenX, screenY, _camera.nearClipPlane));
+
+        // Clamp the mouse position within the screen boundaries
+        mousePosition.x = Mathf.Clamp(mousePosition.x, minScreenBounds.x, maxScreenBounds.x);
+        mousePosition.y = Mathf.Clamp(mousePosition.y, minScreenBounds.y, maxScreenBounds.y);
+
         transform.position = mousePosition;
     }
 }
