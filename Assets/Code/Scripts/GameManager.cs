426d90789268c7bf3731f1d6aa3ef3674f719f5b
Théo <92313024+Sinozer@users.noreply.github.com>
2023-12-14 11:00:25 +0100

    refactor: Moved code to a logical place

--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -5,30 +5,49 @@
 // --------------------------------------- //
 // --------------------------------------- //
 
-using System;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private Transform _player;
-    
+
     private void Start()
     {
-        SetPlayer();
+        FindPlayer();
     }
 
-    public void SetPlayer()
+    private void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode mode)
     {
-        _player = GameObject.FindGameObjectWithTag("Player")?.transform;
+        FindPlayer();
+    }
+
+    private void OnEnable()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void FindPlayer()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+
+        if (go == null)
+            return;
+
+        _player = go.transform;
+    }
+
+    public Transform GetPlayer()
+    {
+        return _player;
     }
 
     public void Quit()
     {
         Application.Quit();
-    }
-    
-    public Transform GetPlayer()
-    {
-        return _player;
     }
 }
