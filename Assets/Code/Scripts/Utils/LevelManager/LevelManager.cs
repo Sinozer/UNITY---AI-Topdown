4fa3a45a1859656e0adcf1a31d21b4019ce1b540
Jun <jzhi@gaming.tech>
2023-12-14 19:27:58 +0100

    feat: implementing LevelStateMachine and LevelManager

---
+++ Assets/Code/Scripts/Utils/LevelManager/LevelManager.cs
@@ -0,0 +1,13 @@
+// --------------------------------------- //
+// --------------------------------------- //
+//  Creation Date: 14/12/23
+//  Description: AI - Topdown
+// --------------------------------------- //
+// --------------------------------------- //
+
+using UnityEngine;
+
+public class LevelManager : Singleton<LevelManager>
+{
+    private LevelStateMachine _levelStateMachine;
+}
