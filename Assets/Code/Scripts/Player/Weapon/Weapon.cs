71203641c33c87b8c50ca8ca9b41e727252d533a
Jun <jzhi@gaming.tech>
2023-12-14 01:12:42 +0100

    Update Weapon.cs

--- Assets/Code/Scripts/Player/Weapon/Weapon.cs
+++ Assets/Code/Scripts/Player/Weapon/Weapon.cs
@@ -11,21 +11,41 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] GameObject bulletPrefab;
 
-    public IShootable shootable;
+    public IShootable Shootable;
     public float BulletSpeed;
-
-    private float _fireRate = 1f;
+    public float FireRate = 2.5f;
+    
+    private float _fireTimer = 0;
+    private bool _canFire = false;
     private Rigidbody2D _bulletRigidbody;
 
     void Start()
     {
+        _fireTimer = 0;
         _bulletRigidbody = bulletPrefab.GetComponent<Rigidbody2D>();
-        shootable = bulletPrefab.GetComponent<IShootable>();
         BulletSpeed = bulletPrefab.GetComponent<Projectile>().Speed;
+        Shootable = bulletPrefab.GetComponent<IShootable>();
+    }
+
+    private void Update()
+    {
+        if (!_canFire)
+        {
+            _fireTimer -= Time.deltaTime;
+            if (_fireTimer <= 0)
+            {
+                _canFire = true;
+            }
+        }
     }
 
     public void Shoot(Vector2 direction)
     {
-        shootable.Shoot(_bulletRigidbody, direction, BulletSpeed);
+        if (_canFire && Shootable != null)
+        {
+            Shootable.Shoot(_bulletRigidbody, direction, BulletSpeed);
+            _canFire = false;
+            _fireTimer = 1 / FireRate;
+        }
     }
 }
