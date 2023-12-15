d74201d6d27f0e2bce6ecf75049abeee9e40e52a
Jun <jzhi@gaming.tech>
2023-12-15 18:00:20 +0100

    feat: Add PatrolTask Node to behavior

---
+++ Assets/Code/Scripts/Entities/ZachScuderi/PatrolTask.cs
@@ -0,0 +1,15 @@
+// --------------------------------------- //
+// --------------------------------------- //
+//  Creation Date: 15/12/23
+//  Description: AI - Topdown
+// --------------------------------------- //
+// --------------------------------------- //
+
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorTree;
+public class PatrolTask : BehaviorTree.Node
+{
+    public PatrolTask(List<Transform> wayPoints) : base() { }
+    
+}
