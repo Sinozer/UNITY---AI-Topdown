8b2d702a04bd35ff619e098731f8a9e3917c1d3e
Jun <jzhi@gaming.tech>
2023-12-15 17:59:39 +0100

    feat: Implementing ZachScuderi

---
+++ Assets/Code/Scripts/Entities/ZachScuderi/ZachBehaviorTree.cs
@@ -0,0 +1,27 @@
+// --------------------------------------- //
+// --------------------------------------- //
+//  Creation Date: 15/12/23
+//  Description: AI - Topdown
+// --------------------------------------- //
+// --------------------------------------- //
+
+using UnityEngine;
+using BehaviorTree;
+
+public class ZachBehaviorTree : BehaviorTree.Tree
+{
+    protected override Node SetupTree()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
