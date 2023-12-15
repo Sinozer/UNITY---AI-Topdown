ad4479d82478200b3c26c6876e6c9e5d6779e910
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2023-12-15 13:08:19 +0100

    refactor: custom destination setter for our game

---
+++ Assets/Code/Scripts/Pathfinding/CustomDestinationSetter.cs
@@ -0,0 +1,42 @@
+// --------------------------------------- //
+// --------------------------------------- //
+//  Creation Date: 15/12/23
+//  Description: AI - Topdown
+// --------------------------------------- //
+// --------------------------------------- //
+
+using Pathfinding;
+using UnityEngine;
+
+public class CustomDestinationSetter : MonoBehaviour
+{
+    private Transform _target;
+    IAstarAI _ai;
+
+    private void OnEnable()
+    {
+        _ai = GetComponent<IAstarAI>();
+
+        if (_ai != null) 
+            _ai.onSearchPath += Update;
+    }
+
+    private void OnDisable()
+    {
+        if (_ai != null) 
+            _ai.onSearchPath -= Update;
+    }
+
+    private void Start()
+    {
+        _target = GameManager.Instance.GetPlayer();
+        if (_target == null)
+            _target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (_target != null && _ai != null) 
+            _ai.destination = _target.position;
+    }
+}
