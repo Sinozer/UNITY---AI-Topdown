84dfb73f4b9396d0783ca8975faf83489a1a0e1e
Styg <56915080+Styyg@users.noreply.github.com>
2023-12-18 18:10:02 +0100

    chore: removed all fu debug

--- Assets/Code/Scripts/Entities/EntityDamageable.cs
+++ Assets/Code/Scripts/Entities/EntityDamageable.cs
@@ -11,17 +11,11 @@ public class EntityDamageable : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.LogWarning("Collision");
-
         if (!collision.gameObject.TryGetComponent<Projectile>(out var projectile))
             return;
 
-        Debug.LogWarning("Projectile");
-
         if (!transform.parent.TryGetComponent<Entity>(out var entity))
             return;
-
-        Debug.LogWarning("Entity");
 
         entity.TakeDamage(projectile.Damage);
     }
