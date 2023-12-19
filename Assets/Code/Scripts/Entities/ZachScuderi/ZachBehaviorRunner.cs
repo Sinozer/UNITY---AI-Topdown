6f5489d4782e42c2ca045e7388968cc52bac078b
Jun <jzhi@gaming.tech>
2023-12-19 23:40:09 +0100

    chore: set enemy data into blackboard and update behavior tree

--- Assets/Code/Scripts/Entities/ZachScuderi/ZachBehaviorRunner.cs
+++ Assets/Code/Scripts/Entities/ZachScuderi/ZachBehaviorRunner.cs
@@ -9,13 +9,16 @@ using UnityEngine;
 
 public class ZachBehaviorRunner : MonoBehaviour
 {
-    private BehaviourTree Tree;
-    
+    public BehaviourTree Tree;
+
+    private Enemy _data;
     void Start()
     {
         GameObject parentGameObject = transform.parent.gameObject;
+        _data = parentGameObject.GetComponentInParent<Enemy>();
         Tree = Tree.Clone();
         Tree.Blackboard.SetValue("Self", parentGameObject);
+        SetDataToBlackboard();
     }
 
     void Update()
@@ -26,6 +29,18 @@ public class ZachBehaviorRunner : MonoBehaviour
     public CustomBlackboard GetBlackboard()
     {
         return Tree.Blackboard;
+    }
+    
+    private void SetDataToBlackboard()
+    {   
+        Tree.Blackboard.SetValue("Health", _data.Health);
+        Tree.Blackboard.SetValue("MaxHealth", _data.MaxHealth);
+        Tree.Blackboard.SetValue("Damage", _data.Damage);
+        Tree.Blackboard.SetValue("MovementSpeed", _data.MovementSpeed);
+        Tree.Blackboard.SetValue("AttackSpeed", _data.AttackSpeed);
+        Tree.Blackboard.SetValue("AttackRange", _data.AttackRange);
+        Tree.Blackboard.SetValue("VisionRange", _data.VisionRange);
+        Tree.Blackboard.SetValue("DistFromPlayer", _data.DistFromPlayer);
     }
     
 }
