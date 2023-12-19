7ae7dbc2968c0fa5f4023076ac6f012c8b6f7fe9
Jun <jzhi@gaming.tech>
2023-12-19 12:52:02 +0100

    feat: Add fade out effect to sprite

--- Assets/Code/Scripts/Entities/ZachScuderi/PatrolNode.cs
+++ Assets/Code/Scripts/Entities/ZachScuderi/PatrolNode.cs
@@ -5,24 +5,41 @@
 // --------------------------------------- //
 // --------------------------------------- //
 
+using System.Collections;
 using UnityEngine;
 
-public class PatrolNode : DecoratorNode
+public class PatrolNode : ActionNode
 {
     private GameObject _self;
+    private SpriteRenderer _sprite;
+    private float _elapsedTime;
+    private float _fadeDuration => 2.0f;
     public override void OnStart()
     {
-       
+        Blackboard.TryFind("Self", out _self);
+        _sprite = _self.GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void OnStop()
     {
-        
+        _sprite.enabled = true;
     }
 
     public override State OnUpdate()
     {
-        Child.Update();
+        if (_self == null)
+            return State.Failure;
+        
+        FadeOut();
         return State.Running;
+    }
+
+    private void FadeOut()
+    {
+        Color color = _sprite.color;
+        _elapsedTime += Time.deltaTime; 
+        float alpha = Mathf.Lerp(1, 0, _elapsedTime);
+        color.a = alpha;
+        _sprite.color = color;
     }
 }
