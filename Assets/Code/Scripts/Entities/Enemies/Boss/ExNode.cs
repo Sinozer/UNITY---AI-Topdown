dd0677f4eef60025bba6d51c593087f82d46f23b
Théo <92313024+Sinozer@users.noreply.github.com>
2023-12-20 18:06:48 +0100

    chore: Started everything for the boss

---
+++ Assets/Code/Scripts/Entities/Enemies/Boss/ExNode.cs
@@ -0,0 +1,26 @@
+// --------------------------------------- //
+// --------------------------------------- //
+//  Creation Date: 20/12/23
+//  Description: AI - Topdown
+// --------------------------------------- //
+// --------------------------------------- //
+
+using UnityEngine;
+
+public class ExNode : ActionNode
+{
+    public override void OnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnStop()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override State OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
