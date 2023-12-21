aaacf55cb5d817de13725169cd628887f24d09f7
Jun <jzhi@gaming.tech>
2023-12-21 15:58:16 +0100

    chore: update BT and prefab structure from hitter branch
    Co-Authored-By: Yoann Rocher <117647468+Evoider@users.noreply.github.com>

--- Assets/Code/Scripts/Entities/Enemies/ZachScuderi/ZachBrain.cs
+++ Assets/Code/Scripts/Entities/Enemies/ZachScuderi/ZachBrain.cs
@@ -10,14 +10,27 @@ using UnityEngine;
 
 public class ZachBrain : EnemyBrain
 {
+    [SerializeField] EnemyBTRunner _runner;
     
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
+        _runner.GetBlackboard().SetValue("AttackSpeed", _entity.AttackSpeed);
     }
 
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+        _canShootAtPlayer = _enemy.DistFromPlayer < _entity.AttackRange;
+        _seePlayer = _enemy.DistFromPlayer < _entity.VisionRange;
+
+        Player player = GameManager.Instance.Player;
+        if (player != null)
+            _runner.GetBlackboard().SetValue("PlayerPosition", (Vector2)GameManager.Instance.Player.transform.position);
+        else
+            _runner.GetBlackboard().SetValue("PlayerPosition", Vector2.zero);
+
+        _runner.GetBlackboard().SetValue("SeePlayer", _seePlayer);
+        _runner.GetBlackboard().SetValue("CanAttack", _canShootAtPlayer);
     }
 }
