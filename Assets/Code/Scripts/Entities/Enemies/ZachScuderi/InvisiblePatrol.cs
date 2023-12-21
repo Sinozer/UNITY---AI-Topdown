21a7d5b2707834f01e75301420c7e5848ce5ddbc
Jun <jzhi@gaming.tech>
2023-12-21 04:00:40 +0100

    chore: Update InvisiblePatrol

--- Assets/Code/Scripts/Entities/Enemies/ZachScuderi/InvisiblePatrol.cs
+++ Assets/Code/Scripts/Entities/Enemies/ZachScuderi/InvisiblePatrol.cs
@@ -10,36 +10,46 @@ using UnityEngine;
 
 public class InvisiblePatrol : ActionNode
 {
-    private GameObject _self;
+    private GameObject _parent;
     private SpriteRenderer _sprite;
     private float _elapsedTime;
-    private float _fadeDuration => 2.0f;
+    private float _fadeDurationScale => 0.5f;
     public override void OnStart()
     {
-        Blackboard.TryFind("Self", out _self);
-        _sprite = _self.GetComponentInChildren<SpriteRenderer>();
+        Blackboard.TryFind("Self", out GameObject _self);
+        _parent = _self.transform.parent.gameObject;
+        _sprite = _parent.GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void OnStop()
     {
-        
+        FadeIn();
     }
 
     public override State OnUpdate()
     {
-        if (_self == null)
+        if (_parent == null)
             return State.Failure;
         
         //FadeOut();
         return State.Running;
     }
 
-    // private void FadeOut()
-    // {
-    //     Color color = _sprite.color;
-    //     _elapsedTime += Time.deltaTime; 
-    //     float alpha = Mathf.Lerp(1, 0, _elapsedTime);
-    //     color.a = alpha;
-    //     _sprite.color = color;
-    // }
+    private void FadeOut()
+    {
+        Color color = _sprite.color;
+        _elapsedTime += Time.deltaTime * _fadeDurationScale; 
+        float alpha = Mathf.Lerp(1, 0, _elapsedTime);
+        color.a = alpha;
+        _sprite.color = color;
+    }
+    
+    private void FadeIn()
+    {
+        Color color = _sprite.color;
+        _elapsedTime += Time.deltaTime * _fadeDurationScale; 
+        float alpha = Mathf.Lerp(0, 1, _elapsedTime);
+        color.a = alpha;
+        _sprite.color = color;
+    }
 }
