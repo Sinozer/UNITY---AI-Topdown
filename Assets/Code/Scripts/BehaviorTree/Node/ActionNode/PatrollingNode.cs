00266720dc587b591c61250ec6ddc6b22773956b
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2023-12-22 01:11:11 +0100

    fix: ranger

--- Assets/Code/Scripts/BehaviorTree/Node/ActionNode/PatrollingNode.cs
+++ Assets/Code/Scripts/BehaviorTree/Node/ActionNode/PatrollingNode.cs
@@ -32,11 +32,7 @@ public class PatrollingNode : ActionNode
             return State.Failure;
 
         if (!_brain.SeePlayer)
-        {
-            _brain?.Patrolling(true);
-            _brain?.AIPath(true);
             return State.Running;
-        }
 
         Debug.Log("");
         return State.Success;
